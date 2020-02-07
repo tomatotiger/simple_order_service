f2762ba89ecd543c01672de11ecc8c6e7ce7884c
Judy <yanghuanghuang@gmail.com>
2020-02-07 14:07:00 -0800

    add create_order and cancle_order function to model Order

--- inventory/models.py
+++ inventory/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.db import IntegrityError, transaction
 
 
 CREATED = 'created'
-CANCELEU = 'canceled'
+CANCELED = 'canceled'
 IN_PROCESSING = 'in_processing'
 DELIVERED = 'delivered'
 ORDER_STATUS = [
     (CREATED, 'Created'),
     (IN_PROCESSING, 'In Processing'),
-    (CANCELEU, 'Canceled'),
+    (CANCELED, 'Canceled'),
     (DELIVERED, 'Delivered'),
 ]
 
@@ -22,12 +23,33 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class OrderManager(models.Manager):
+    def cancel_order(self):
+        with transaction.atomic():
+            self.order_status = CANCELED
+            for product in self.products.all():
+                product.quantity_available += 1
+                product.save()
+            self.save()
+
+    def create_order(self, **kwargs):
+        with transaction.atomic():
+            products = kwargs.pop('products')
+            order = self.create(**kwargs)
+            order.products.set(products)
+            for product in order.products.all():
+                product.quantity_available -= 1
+                product.save()
+            return order
+
+
 class Order(models.Model):
     products = models.ManyToManyField(Product)
     customer_email_address = models.EmailField()
     date_order_placed = models.DateTimeField(auto_now_add=True)
     date_order_updated = models.DateTimeField(auto_now=True)
     order_status = models.CharField(max_length=50, choices=ORDER_STATUS)
+    objects = OrderManager()
 
 
 class OrderLog(models.Model):
